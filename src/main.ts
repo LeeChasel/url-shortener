a5ad442def34d510297fd5108a44e24d0a31c11b
Chasel <arko9988b@gmail.com>
2025-11-13 02:06:55 +0800

    feat: setup global dto validation feature

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from './libs';
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +11,13 @@ async function bootstrap() {
     defaultVersion: '1',
     prefix: 'v',
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   const configSerice = app.get(ConfigService);
   await app.listen(configSerice.get('APP_PORT'));
